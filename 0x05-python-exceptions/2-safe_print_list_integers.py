16eeaab64de8713e7a3534bb7b9e0c57e9ec172b
Leo Maina <leonardhophni@gmail.com>
2023-03-29 12:53:24 -0700

    Initial commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    i = 0
     while (i < x):
         try:
-            if(my_list[i].isdigit()):
-                print("{:d}".format(my_list[i]))
-                count += 1
+            print("{:d}".format(my_list[i]))
+            count += 1
         except(IndexError, ValueError, TypeError):
                 break
-            i += 1
         print("")
         return count
