6c20f8c499f4711f86659b5c7dc7596b843f84ad
Leo Maina <leonardhophni@gmail.com>
2023-03-29 12:02:39 -0700

    Initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    i = 0
 
-    for i in range(x):
+    while(i < x):
         try:
             print("{}".format(my_list[i]), end='')
         except:
             break
-        else:
-            count += 1
-
-    print()
-    return (count)
+        i = i+1
+    return i
