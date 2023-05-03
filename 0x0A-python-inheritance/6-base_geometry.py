4b85f9d70926b32e1ed4b17a9dc9927d45abdb25
Leo Maina <leonardhophni@gmail.com>
2023-05-03 11:43:33 -0700

    Initial commit

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Class: 
+Class:
     BaseGeometry: defines a base geometry
 """
 
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Class: 
+Class:
     Rectangle: inherits from BaseGeometry class
 """
 
